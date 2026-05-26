b5501ac00e5958bbd65433b9dc77c71872e7d4bc
ITSDeniz <denizuguz18@gmail.com>
2026-05-27 00:38:29 +0200

    Fix: Restore working core functions

--- geometry.py
+++ geometry.py
@@ -7,7 +7,7 @@ def triangle_area(base, height):
 
 def square_area(side):
     """Calculates the area of a square."""
-    return side ** 5  
+    return side ** 2
 
 if __name__ == "__main__":
     print(f"Triangle Area: {triangle_area(10, 5)}")
